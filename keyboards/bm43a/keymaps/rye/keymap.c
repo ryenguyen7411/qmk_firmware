fe9cfa90220f49a400c0173cdbbb302ffe28ddda
Rye Nguyen <ryenguyen7411@gmail.com>
2021-06-20 17:41:56 +0700

    UPDATE: enhance some vim keycode.

--- keyboards/bm43a/keymaps/rye/keymap.c
+++ keyboards/bm43a/keymaps/rye/keymap.c
@@ -33,9 +33,7 @@ enum custom_keycodes {
   MC_VIM_07,
   MC_VIM_08,
   MC_VIM_09,
-  MC_VIM_10,
-  MC_VIM_12,
-  MC_VIM_13,
+  MC_VIM_11,
 };
 
 enum tap_dances {
@@ -48,7 +46,7 @@ enum tap_dances {
   TD_COMM,
   TD_DOT,
 
-  TD_VIM_11,
+  TD_VIM_12,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LALT, KC_LGUI, LT(FN3, KC_SPC), LT(FN4, KC_SPC), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
   ),
   [FN1] = LAYOUT(
-    _______, _______, _______, TD(TD_VIM_11), MC_VIM_10, _______, _______, MC_VIM_12, MC_VIM_13, KC_LCBR, KC_RCBR, KC_DEL,
+    _______, _______, _______, TD(TD_VIM_12), MC_VIM_11, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_DEL,
     _______, KC_WH_R, KC_WH_U, KC_WH_D, KC_WH_L, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, C(KC_SPC),
     _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______, C(KC_LEFT), C(KC_RIGHT), C(KC_UP),
     _______, _______, _______, _______, _______, _______, _______, _______, _______
@@ -71,15 +69,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
   [FN3] = LAYOUT(
-    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, TD(TD_LBRC), TD(TD_RBRC),
-    KC_LGUI, KC_LSFT, KC_LALT, KC_GRV, KC_TAB, _______, TD(TD_MINS), TD(TD_PLUS), TD(TD_COLN), TD(TD_QUOT), KC_BSLS,
-    _______, _______, _______, _______, _______, _______, _______, TD(TD_COMM), TD(TD_DOT), KC_SLSH, _______,
+    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRC, KC_RBRC,
+    KC_LGUI, KC_LSFT, KC_LALT, KC_GRV, KC_TAB, C(S(KC_6)), KC_MINS, KC_EQL, TD(TD_COLN), TD(TD_QUOT), KC_BSLS,
+    _______, _______, _______, _______, _______, _______, _______, KC_COMM, KC_DOT, KC_SLSH, G(KC_V),
     _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
   [FN4] = LAYOUT(
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-    _______, _______, _______, _______, MC_VIM_05, MC_VIM_09, _______, MC_VIM_03, MC_VIM_06, MC_VIM_02, MC_VIM_01,
-    _______, _______, _______, _______, _______, MC_VIM_04, _______, MC_VIM_07, MC_VIM_08, _______, _______,
+    MC_VIM_05, _______, _______, _______, _______, MC_VIM_09, _______, MC_VIM_03, MC_VIM_06, MC_VIM_02, MC_VIM_01,
+    _______, _______, _______, _______, _______, MC_VIM_04, _______, _______, MC_VIM_07, MC_VIM_08, G(KC_V),
     _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 };
@@ -212,18 +210,13 @@ void tapdance_dot_reset(qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
-void tapdance_vim_11_finished(qk_tap_dance_state_t *state, void *user_data) {
+void tapdance_vim_12_finished(qk_tap_dance_state_t *state, void *user_data) {
   if (state->count > 1) {
-    register_code(KC_COMM);
-    unregister_code(KC_COMM);
-    register_code(KC_Q);
-    unregister_code(KC_Q);
-    register_code(KC_Q);
-    unregister_code(KC_Q);
+    SEND_STRING(SS_TAP(X_ESC) SS_DELAY(50) SS_TAP(X_SPACE) "qq");
   }
 }
 
-void tapdance_vim_11_reset(qk_tap_dance_state_t *state, void *user_data) {
+void tapdance_vim_12_reset(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 // All tap dance functions would go here.
@@ -236,7 +229,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_RBRC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tapdance_rbrc_finished, tapdance_rbrc_reset),
   [TD_COMM] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tapdance_comm_finished, tapdance_comm_reset),
   [TD_DOT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tapdance_dot_finished, tapdance_dot_reset),
-  [TD_VIM_11] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tapdance_vim_11_finished, tapdance_vim_11_reset),
+  [TD_VIM_12] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tapdance_vim_12_finished, tapdance_vim_12_reset),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -244,22 +237,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case MC_VIM_01:
       if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_COMM) "ff");
+        SEND_STRING(SS_TAP(X_ESC) SS_DELAY(50) SS_TAP(X_SPACE) ",;");
       }
       break;
     case MC_VIM_02:
       if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_COMM) "fp");
+        SEND_STRING(SS_TAP(X_ESC) SS_DELAY(50) SS_TAP(X_SPACE) ",l");
       }
       break;
     case MC_VIM_03:
       if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_COMM) "fg");
+        SEND_STRING(SS_TAP(X_ESC) SS_DELAY(50) SS_TAP(X_SPACE) ",j");
       }
       break;
     case MC_VIM_04:
       if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_COMM) "fb");
+        SEND_STRING(SS_TAP(X_ESC) SS_DELAY(50) SS_TAP(X_SPACE) ",b");
       }
       break;
     case MC_VIM_05:
@@ -269,37 +262,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case MC_VIM_06:
       if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_COMM) "fx");
+        SEND_STRING(SS_TAP(X_ESC) SS_DELAY(50) SS_TAP(X_SPACE) ",k");
       }
       break;
     case MC_VIM_07:
       if (record->event.pressed) {
-        SEND_STRING(":vs" SS_TAP(X_ENT));
+        SEND_STRING(SS_TAP(X_ESC) SS_DELAY(50) "[g");
       }
       break;
     case MC_VIM_08:
       if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("w") "q");
+        SEND_STRING(SS_TAP(X_ESC) SS_DELAY(50) "]g");
       }
       break;
     case MC_VIM_09:
       if (record->event.pressed) {
-        SEND_STRING("j" SS_TAP(X_TAB));
+        SEND_STRING(",g");
       }
       break;
-    case MC_VIM_10:
+    case MC_VIM_11:
       if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_COMM) "w");
-      }
-      break;
-    case MC_VIM_12:
-      if (record->event.pressed) {
-        SEND_STRING("5j");
-      }
-      break;
-    case MC_VIM_13:
-      if (record->event.pressed) {
-        SEND_STRING(SS_TAP("5k"));
+        SEND_STRING(SS_TAP(X_ESC) SS_DELAY(50) SS_TAP(X_SPACE) "w");
       }
       break;
     default:
